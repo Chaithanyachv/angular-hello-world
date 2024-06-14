d834266fa8a087defcf83e86be71a5b2ad707d77
chaitanyachenna93 <chaitanyachenna93@gmail.com>
2024-06-14 11:25:33 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { RegistationComponent } from './registation/registation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    RegistationComponent
   ],
   imports: [
     BrowserModule,
